a0ed62615ccb4d47c0f64965027a7bc5c74a3e8b
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-14 09:14:49 +0530

    login created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup from './components/Signup';
+import Login from './Login';
 
 function App() {
   return (
     <div>
       <Signup/>
+      <Login/>
     </div>
   );
 }
