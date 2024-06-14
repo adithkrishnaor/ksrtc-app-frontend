f2d348b9514a4d32f2b00e8558582e97f25fd735
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-14 22:51:49 +0530

    login modified

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Navbar from './Navbar'
 
 const Login = () => {
 
+    const navigate=useNavigate()
     const [data, setdata] = useState({
         "email": "",
         "pass": ""
@@ -16,6 +18,7 @@ const Login = () => {
             (response) => {
                 if (response.data.status == "success") {
                     alert("Login Success")
+                    navigate("/add")
                 }
                 else {
                     alert("Login Failed")
@@ -31,6 +34,7 @@ const Login = () => {
 
     return (
         <div>
+            <Navbar/>
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -45,7 +49,7 @@ const Login = () => {
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <center>
-                                <button className="btn btn-success" onClick={readValue}>Login</button><br />
+                                <button className="btn btn-success" onClick={readValue}>Login</button><br /> <br />
                                 <a href="/signup">Signup</a>
                                 </center>
                             </div>
