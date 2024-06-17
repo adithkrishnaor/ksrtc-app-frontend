c1c99b763f6194dad4bc17a1cab19c74b9cb5797
Adith <147383680+adithkrishnaor@users.noreply.github.com>
2024-06-17 18:54:08 +0530

    Update Signup.jsx
    navbar disabled

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () => {
 
     return (
         <div>
-            <Navbar/>
+{/*             <Navbar/> */}
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
